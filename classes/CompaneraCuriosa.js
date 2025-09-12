819d590c4396785afc6453d7bded3fba4bdec0bb
KarolainReyes <reyeskarol0412@gmail.com>
2025-09-12 10:58:12 -0500

    feat: :bricks: Created the CompaneraCuriosa subclass
    The structure for the CompaneraCuriosa class was created
    
    by @KarolainReyes

---
+++ classes/CompaneraCuriosa.js
@@ -0,0 +1,16 @@
+const Chismosa = require('./Chismosa');
+
+class CompaneraCuriosa extends Chismosa {
+    constructor(nombre, reputacion,nivelChisme){
+        super(nombre,reputacion,nivelChisme);
+    }
+    recolectarInfo(){
+        console.log("Hace preguntas aparentemente profesionales a los compañeros de trabajo");
+    }
+    contarChisme(){
+        console.log("Comenta el chisme como ejemplo de vida en el trabajo")
+    }
+    
+    
+
+}
